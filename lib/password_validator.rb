7e4c796cd7fd495665d927aa7a6461ddcf69739e
Tanya Sahin <tanyasahin123@gmail.com>
2022-11-04 11:15:26 +0000

    First commit

---
+++ lib/password_validator.rb
@@ -0,0 +1,12 @@
+# Valid passowords are
+# - 8 or more characters
+# - include at least one of these characters: !, @, $, %, &
+
+def valid?(password)
+    specialchar = /[!@$%&]/
+    if password =~ specialchar && password.length > 7
+        return true
+    else
+        return false
+    end
+end
